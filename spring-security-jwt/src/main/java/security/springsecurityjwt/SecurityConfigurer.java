24466bb2293d6f27c70da2914c44155911b7aecb
Kacper <kacpergierycz@gmail.com>
2021-06-24 12:54:57 +0200

    Resource hello, SecurityConfigurer and MyuserDService

---
+++ spring-security-jwt/src/main/java/security/springsecurityjwt/SecurityConfigurer.java
@@ -0,0 +1,23 @@
+package security.springsecurityjwt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import security.springsecurityjwt.services.MyUserDetailsService;
+
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private MyUserDetailsService myUserDetailsService;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		
+		auth.userDetailsService(myUserDetailsService);
+	}
+
+	
+	
+}
